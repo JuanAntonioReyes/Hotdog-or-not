058ebf26c648d31b78c065640c1f6e67248440a0
Juan Antonio Reyes <biballo146@gmail.com>
2018-08-02 20:20:59 +0200

    Prediction paragraph changes if the image is hotdog or not

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 let image = document.getElementById('image');
 let loadingP = document.getElementById('loading');
+let predictionP = document.getElementById('prediction');
 
 // Initialize the image classifier MobileNet
 const classifier = ml5.imageClassifier('MobileNet', function() {
 	loadingP.innerText = "Prediction model succesfully loaded.";
 });
 
-// Make a prediction with the image
-classifier.predict(image, function(err, results) {
-	console.log("Prediction: " + results[0].className);
-});
+// Check if the image is a hotdog
+classifier.predict(image, checkHotdog);
+
+function checkHotdog(err, data) {
+	let prediction = data[0].className;
+
+	let isHotdog = ( prediction === "hotdog, hot dog, red hot" );
+
+	if (isHotdog) {
+		predictionP.innerText = "YES!";
+	} else {
+		predictionP.innerText = "NO";
+	}
+}
