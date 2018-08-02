ef600b324c633905875af6de812037c43551022d
Juan Antonio Reyes <biballo146@gmail.com>
2018-08-02 20:03:45 +0200

    classifier test

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("THIS WORKS!");
+let image = document.getElementById('image');
+let loadingP = document.getElementById('loading');
+
+// Initialize the image classifier MobileNet
+const classifier = ml5.imageClassifier('MobileNet', function() {
+	loadingP.innerText = "Prediction model succesfully loaded.";
+});
+
+// Make a prediction with the image
+classifier.predict(image, function(err, results) {
+	console.log("Prediction: " + results[0].className);
+});
