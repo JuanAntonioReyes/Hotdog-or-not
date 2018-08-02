04289433f99d9a26a26972d2eff01aaccf215002
Juan Antonio Reyes <biballo146@gmail.com>
2018-08-02 20:50:22 +0200

    Image selection

--- js/app.js
+++ js/app.js
@@ -1,14 +1,37 @@
-let image = document.getElementById('image');
-let loadingP = document.getElementById('loading');
-let predictionP = document.getElementById('prediction');
+let loadingP = document.getElementById("loading");
+let predictionP = document.getElementById("prediction");
+
+// SEt the file input onChange listener
+let imageInput = document.getElementById("imageInput");
+imageInput.addEventListener("change", handleImage);
 
 // Initialize the image classifier MobileNet
-const classifier = ml5.imageClassifier('MobileNet', function() {
+const classifier = ml5.imageClassifier("MobileNet", function() {
 	loadingP.innerText = "Prediction model succesfully loaded.";
 });
 
-// Check if the image is a hotdog
-classifier.predict(image, checkHotdog);
+function handleImage() {
+	let imageTag = document.getElementById("image");
+	let image = this.files[0];
+	let reader = new FileReader();
+
+	// This function will execute when we call "readAsDataURL"
+	reader.onloadend = function () {
+		// Set the img src to the image
+		imageTag.src = reader.result;
+
+		// Check if the image is a hotdog
+		classifier.predict(imageTag, checkHotdog);
+	}
+
+	if (image) {
+		reader.readAsDataURL(image);
+	} else {
+		imageTag.src = "";
+	}
+
+	
+}
 
 function checkHotdog(err, data) {
 	let prediction = data[0].className;
